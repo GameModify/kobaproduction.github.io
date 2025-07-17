04f99f4dabc7e82a0266ef8a51fbcd02650b2475
GameModify <serent2003@mail.ru>
2025-07-17 15:54:07 +0300

    fix(App.vue): added i18n usage

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { Dark, useQuasar } from 'quasar'
 
@@ -10,6 +10,7 @@ import { DefaultHeader } from '@/app/ui/headers'
 import { DefaultSideBar } from '@/app/ui/sidebars'
 import { DefaultFooter } from '@/app/ui/footers'
 import { DefaultLayout } from '@/app/ui/layouts'
+import { useI18n } from 'vue-i18n'
 
 const store = useAppStore()
 const tgWebApp = useWebApp()
@@ -29,7 +30,14 @@ if (typeof useWebAppViewport === 'function' && useQuasar().platform.is.mobile) {
 const styles = computed(() => Dark.isActive ? 'text-white bg-dark' : 'text-dark bg-white')
 const route = useRoute()
 const layout = computed(() => route?.meta?.layout || DefaultLayout)
+const i18n = useI18n()
 
+i18n.locale.value = store.settings.language
+
+watch(store.settings, () => {
+  if (i18n.locale.value !== store.settings.language) {
+    i18n.locale.value = store.settings.language
+  }})
 </script>
 
 <template>
