9b6baeefc697ec14eccedf2745a32d93154f0cd7
GameModify <serent2003@mail.ru>
2025-07-17 14:13:12 +0300

    Update i18n translation text

--- src/shared/i18n/en-US/index.ts
+++ src/shared/i18n/en-US/index.ts
@@ -1,7 +1,29 @@
 const enUS = {
-  basedOn: 'Based On',
-  oopsNothingHere: 'Oops. Nothing here...',
-  goToIndexPage: 'Go to index page',
+  errorPages: {
+    oopsNothingHere: 'Oops. Nothing here...',
+    goToIndexPage: 'Go to index page',
+    errorPageCode: 'Error page code',
+  },
+  pagesTabs: {
+    index: 'Home',
+    settings: 'Settings',
+  },
+  settings: {
+    app: {
+      title: 'Application Settings',
+      darkModeAuto: 'Auto Dark Mode',
+      darkModeAutoCaption: 'Automatic installation of a dark mode',
+      darkMode: 'Dark Mode',
+      darkModeCaption: 'Switching the dark mode',
+      language: 'Language',
+      languageCaption: 'Switching the application language',
+    },
+    backend: {
+      title: 'Backend server Settings',
+      apiToken: 'API Token',
+      apiTokenCaption: 'Access key to the API server with data',
+    },
+  },
 }
 
 export default enUS
--- src/shared/i18n/ru-RU/index.ts
+++ src/shared/i18n/ru-RU/index.ts
@@ -1,7 +1,29 @@
 const ruRU = {
-  basedOn: 'Основано на',
-  oopsNothingHere: 'Уупс. Тут ничего нет...',
-  goToIndexPage: 'Вернуться на главную',
+  errorPages: {
+    oopsNothingHere: 'Уупс. Тут ничего нет...',
+    goToIndexPage: 'Вернуться на главную',
+    errorPageCode: 'Код страницы ошибки',
+  },
+  pagesTabs: {
+    index: 'Главная',
+    settings: 'Настройки',
+  },
+  settings: {
+    app: {
+      title: 'Настройки приложения',
+      darkModeAuto: 'Авто темный режим',
+      darkModeAutoCaption: 'Автоматическое переключение темного режима',
+      darkMode: 'Темный режим',
+      darkModeCaption: 'Переключение темного режима',
+      language: 'Язык',
+      languageCaption: 'Переключение языка приложения',
+    },
+    backend: {
+      title: 'Настройки серверной части',
+      apiToken: 'API Токен',
+      apiTokenCaption: 'Ключ доступа к API серверу с данными',
+    },
+  },
 }
 
 export default ruRU
